da6e8277dfec429942ffa569abbe92b20ce2c863
Thapelo Boya <88501835+ThapeloBoya@users.noreply.github.com>
2023-10-17 13:30:22 +0200

    Add files via upload

---
+++ primera capital/about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Primera Capital | About</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="corporate finance and advisory, primera capital, Assisting shareholder value preservation and growth throughout the economic cycle" />
+<meta name="description" content="Primera is founded on integrity and sound corporate governance. We ensure the highest standards of ethics in all our dealings with Clients, Funders, Investors and ..." />
+<meta name="robots" content="noindex" />
+<link rel="icon" href="https://d1ujqdpfgkvqfi.cloudfront.net/favicon-generator/htdocs/favicons/2020-11-07/93889ed146ac051698b9aacba97a77f7.ico.png">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/jQuery.lightninBox.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/team.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/portfolio.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/services.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/pogo-slider.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/nav.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/aos.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" media="all" rel="stylesheet" type="text/css">
+<script src="js/modernizr.js"></script>
+<style type="text/css">
+.header {
+padding: 100px;
+text-align: center;
+background: url(images/team.jpg) no-repeat;
+background-size: cover;
+color: white;
+font-size: 30px;
+}
+</style>
+</head>
+<body>
+<?php include 'includes/header.php';?>
+<div class="header">
+  <h1></h1>
+  <p></p>
+</div>
+
+<!-- about us Section -->
+<div class="container" data-aos="zoom-in" style="padding: 70px 0 0 40px;">
+  <div class="row">
+    <div class="column-66">
+<h4 style="border-left: #1C164B solid 4px; font-size: 35px; color:#707070; text-transform: uppercase; padding: 0 0 5px 7px; font-weight: bold;text-align: left;">Who We Are</h4>
+      <p style="font-size: 16px; line-height: 2;text-align: justify;padding: 20px 0 0 0;color: #808080;">Our transaction experience covers a period of more than 20 years in a breadth of corporate finance and investment management including debt and equity capital raising, mergers and acquisitions (“M&A”), Black Economic Empowerment (“BEE”) transaction structuring, Leveraged Buy-Outs, Management Buy-outs and Project Finance. We are sector agnostic however our experience includes a range of sectors including financial services, telecoms media and technology (“TMT”), infrastructure, property, industrials, healthcare and food and beverage.<br><br>Our focus is on serving privately owned businesses facilitating their growth and success through our services and expert advice. We are also ideally positioned to assist shareholders and directors dealing with the impact of Covid-19 and the resultant economic impact on their businesses through our Turnaround Management and Business Rescue service offering. Our focus is on businesses with a turnover of R100 million and above.</p>
+    </div>
+    <div class="column-33">
+        <img src="images/l1.jpg" alt="primera capital about us" style="width: 450px; height: 400px">
+    </div>
+  </div>
+</div>
+<!-- about us Section -->
+<!-- ethos Section -->
+<div class="container" data-aos="zoom-in" style="padding: 70px 0 0 40px;">
+  <div class="row">
+    <div class="column-33">
+      <img src="images/l2.jpg" alt="primera capital about us" style="width: 450px;height: 450px;">
+    </div>
+    <div class="column-66">
+    <h4 style="border-left: #1C164B solid 4px; font-size: 35px; color:#707070; text-transform: uppercase; padding: 0 0 5px 7px; font-weight: bold;text-align: left;">Our Ethos</h4><br><br>
+      <p style="font-size: 16px; line-height: 2;text-align: justify;padding: 20px 0 0 0;color: #808080;">We are an independent firm which allows us to be a trusted advisor to our clients. We are not exclusively tied to any debt funder or equity investor but rather have established relationships with a broad range of Commercial Banks, Development Finance Institutions (“DFI’s”), Private Equity and Venture Capital Investors, BEE groups, Investment holding companies, Family Offices and High Net Worth Individuals.<br><br>We also collaborate with other investment banking professionals, Turnaround management professionals and industry experts to ensure skills and capacity are leveraged from our network to the benefit of our clients.Our independence, wealth of experience and extensive knowledge of private debt and equity capital markets enable us to secure the best available funding terms and most suitable equity partner for our clients.<br><br> Whether it be in a sale to a private equity investor, a BEE group or as part of a management Buy-in or Management Buy-out. Our collaborative approach and extensive C-suite leadership experience makes us ideally positioned to assist on Turnarounds and Business Rescue projects.</p>
+    </div>
+  </div>
+</div>
+<!-- ethos Section -->
+<!-- values -->
+<div class="bannerbottom" data-aos="zoom-in" id="about" style="padding:80px 0 50px 0;">
+<div class="container">
+<div class="serviceicons">
+<h4 style="border-left: #1C164B solid 4px; font-size: 35px; color:#707070; text-transform: uppercase; padding: 0 0 5px 7px; font-weight: bold;">Our Values</h4><br><br>
+<div class="col-md-3">
+<div class="service1">
+<h5 style="  text-align: left;font-size: 25px; color:#1C164B;padding-bottom: 10px;font-weight: bold;  " class="top-and-bottom">We are client centric<br></h5>
+<p style="color: #808080;padding: 20px 0 0 0;font-size: 16px;text-align: justify;">We strive to leverage our skills, knowledge and extensive networks to ensure that our Client’s achieve their business objectives.</p>
+</div>
+</div>
+<div class="col-md-3">
+<div class="service1">
+<h5 style="  text-align: left;font-size: 25px; color:#1C164B;padding-bottom: 10px; font-weight: bold; " class="top-and-bottom">Trusted relationships</h5>
+<p style="color: #808080;padding: 20px 0 0 0;font-size: 16px;text-align: justify;">Our network of trusted relationships within the investment banking industry and with our valued clients is the cornerstone of our business.</p>
+</div>
+</div>
+<div class="col-md-3">
+<div class="service1">
+<h5 style="  text-align: left;font-size: 25px; color:#1C164B;padding-bottom: 10px; font-weight: bold; " class="top-and-bottom">Stand for excellence<br> </h5>
+<p style="color: #808080;padding: 20px 0 0 0;font-size: 16px;">We partner with other like-minded professionals to ensure that our Clients benefit from cutting edge research, technology and sector expertise.</p>
+</div>
+</div>
+<div class="col-md-3">
+<div class="service1">
+<h5 style="  text-align: left;font-size: 25px; color:#1C164B;padding-bottom: 10px;font-weight: bold;  " class="top-and-bottom">Good corporate citizen </h5>
+<p style="color: #808080;padding: 20px 0 0 0;font-size: 16px;text-align: justify;">We ensure the highest standards of ethics in all our dealings with Clients, Funders, Investors and Partners.</p>
+</div>
+</div>
+<div class="clearfix"></div>
+</div>
+</div>
+</div>
+<br><br>
+<!-- values Section -->
+<?php include 'includes/footer.php';?>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src='js/aos.js'></script>
+<script src="js/lazyload.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/grayscale.js"></script>
+<script src="js/jarallax.js"></script>
+<script type="text/javascript">
+$('.jarallax').jarallax({
+speed: 0.5,
+imgWidth: 1680,
+imgHeight: 925
+})
+</script>
+<script src="js/main2.js"></script>
+<script src="js/jQuery.lightninBox.js"></script>
+<script type="text/javascript">
+$(".lightninBox").lightninBox();
+</script>
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/classie2.js"></script>
+<script src="js/cbpGridGallery.js"></script>
+<script>
+new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+</script>
+<script type="text/javascript" src="js/jquery.slide.js"></script>
+<script type="text/javascript">
+$('#slideshow-banner').slide({
+cdTime : 5000,      
+controllerLeftButton : "images/left.png",   
+controllerRightButton : "images/right.png"   
+});
+</script>
+<script src="js/jquery.pogo-slider.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/demo1.js"></script>
+</body>
+</html>
